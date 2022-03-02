50f3e73141fb8416113cbd1843e37920d070bca0
annonemys3333 <100736755+annonemys3333@users.noreply.github.com>
2022-03-02 03:33:43 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,13 +4,13 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "Test",
+    host: "roundhead.aternos.host:44048",
+    port: "44048",
+    username: "fanti",
     version: false
     })
     bot.on('login', function() {
-      bot.chat('/reginster 123123123 123123123')
+      bot.chat('/reginster fanti 123123123 123123123')
     })
     bot.on('chat', (username, message) => {
       if (username === bot.username) return
